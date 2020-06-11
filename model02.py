cb564cb312d47c46bd838647f17ba8c9016a31e2
unknown <robertojuncos@wengan.com.ar>
2020-06-11 16:48:42 -0300

    fix waypoints def

--- model02.py
+++ model02.py
@@ -1,49 +1,60 @@
+import math
+
+
 def reward_function(params):
-    
-    #waypoints
+
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    all_wheels_on_track = params['all_wheels_on_track']
+    is_left_of_center = params['is_left_of_center']
+    steering_angle = params['steering_angle']
+    speed = params['speed']
+
+
+#waypoints
     waypoints = params['waypoints']
     closest_waypoints = params['closest_waypoints']
     
     next_point = waypoints[closest_waypoints[1]]
-    prev_point = closest_waypoints[0]
+    prev_point = waypoints[closest_waypoints[0]]
+
+
     
-    #distance from center
-    distance_from_center = params['distance_from_center']
-    track_width = params['track_width']
-    steering = abs(params['steering_angle']) # Only need the absolute steering angle
+    if is_left_of_center == True:
+        distance_from_center *= -1
+
+    # implementation of reward function for distance from center
+    reward = (1 / (math.sqrt(2 * math.pi * (track_width*2/15) ** 2)) * math.exp(-((
+            distance_from_center + track_width/20) ** 2 / (4 * track_width*2/15) ** 2))) *(track_width*1/3)
     
-    #speed
-    speed = params['speed']
-
-    # Calculate 3 markers that are at varying distances away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
-
-    # Give higher reward if the agent is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-        reward = 1
-    elif distance_from_center <= marker_2:
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-        reward = 0.1
-    else:
-        reward = 1e-3  # likely crashed/ close to off track
-
-    # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 15
-
-    # Penalize reward if the agent is steering too much
-    if steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
+    if not all_wheels_on_track:
+        reward = 1e-3
+    else:  
+        # implementation of reward function for steering angle
+        STEERING_THRESHOLD = 14.4
         
-    if prev_point in range(1,6) or prev_point in range(24,28):
-        if speed < 2:
-            reward *=0.5
-        elif speed < 3:
-            reward *=0.6
-        elif speed < 4:
-            reward *=0.8        
-            
+        if abs(steering_angle) < STEERING_THRESHOLD:
+            steering_reward = math.sqrt(- (8 ** 2 + steering_angle ** 2) + math.sqrt(4 * 8 ** 2 * steering_angle ** 2 + (12 ** 2) ** 2) ) / 10
+        else:
+            steering_reward = 0
 
+        # aditional reward if the car is not steering too much
+        reward *= steering_reward
+
+        # reward for the car taking fast actions (speed is in m/s)
+        reward *= math.sin(speed/math.pi * 5/6)
+        
+        # same reward for going slow with greater steering angle then going fast straight ahead 
+        reward *= math.sin(0.4949 * (0.475 * (speed - 1.5241) + 0.5111 * steering_angle ** 2))
+
+
+        #Because Max speed is 1 and granularity is 3 (0.33/0.67/1.00)
+            if prev_point in range(1,6) or prev_point in range(24,28):
+                if speed <= 0.33:
+                    reward *=0.3
+                elif speed <= 0.67:
+                    reward *=0.5
+                else:
+                    reward = reward + 1.0 
     return float(reward)
