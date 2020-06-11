2a2ab6fd47d6ecfdc17cbe6cec0650a826b09283
unknown <robertojuncos@wengan.com.ar>
2020-06-11 15:19:09 -0300

    new models

---
+++ model02.py
@@ -0,0 +1,49 @@
+def reward_function(params):
+    
+    #waypoints
+    waypoints = params['waypoints']
+    closest_waypoints = params['closest_waypoints']
+    
+    next_point = waypoints[closest_waypoints[1]]
+    prev_point = closest_waypoints[0]
+    
+    #distance from center
+    distance_from_center = params['distance_from_center']
+    track_width = params['track_width']
+    steering = abs(params['steering_angle']) # Only need the absolute steering angle
+    
+    #speed
+    speed = params['speed']
+
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+
+    # Give higher reward if the agent is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+
+    # Steering penality threshold, change the number based on your action space setting
+    ABS_STEERING_THRESHOLD = 15
+
+    # Penalize reward if the agent is steering too much
+    if steering > ABS_STEERING_THRESHOLD:
+        reward *= 0.8
+        
+    if prev_point in range(1,6) or prev_point in range(24,28):
+        if speed < 2:
+            reward *=0.5
+        elif speed < 3:
+            reward *=0.6
+        elif speed < 4:
+            reward *=0.8        
+            
+
+    return float(reward)
